13c5d80663fd3510f8dd6e2aca28a99bd73c31af
umerjaved123 <donumer67@gmail.com>
2024-02-07 00:52:07 +0500

    html learning part 1
    this is the html learning part 1

---
+++ Learning task no 1/script.js
@@ -0,0 +1,5 @@
+// How to put alerts in JavaScript is mentioned below ....
+
+alert("welcome to sigma web development course")
+
+alert("yes its the javascript bro and it will be implemented in script tag")

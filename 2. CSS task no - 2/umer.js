e11ff9a7ce0281e9912ad93bee19a08f9bf98c51
umerjaved123 <donumer67@gmail.com>
2024-02-15 16:13:22 +0500

    CSS
    CSS

---
+++ 2. CSS task no - 2/umer.js
@@ -0,0 +1,14 @@
+// alert(),function umer(params) {
+//     console.log("onclick");
+// }
+
+
+// alert("simple css practise website")
+
+
+// alert("this is just for practise purposes")
+  
+// alert();{
+
+//     console.log("this is my website")
+// }
